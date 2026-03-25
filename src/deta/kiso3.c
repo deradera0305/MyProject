aa7648552a8799119d584df6ec7153befc03ae57
deradera0305 <dera200235@icloud.com>
2026-03-26 00:06:46 +0900

    printfの書式設定の作成

---
+++ src/deta/kiso3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+    printf(" %8s %8s\n", " 商品A", "商品B");
+    printf("数量%08d %08d\n", 16, 256);
+    printf("重量%8.4f %8.4f\n", 75.6, 555.1);
+
+    return 0;
+}
